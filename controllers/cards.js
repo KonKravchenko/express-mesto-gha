2357f97f1864f68eb15fdf1a22942e7ef356365a
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-23 02:28:03 +0300

    Спринт 14

--- controllers/cards.js
+++ controllers/cards.js
@@ -34,21 +34,20 @@ module.exports.deleteCard = (req, res, next) => {
   Card.findById(cardId)
     .orFail(new Error('NotValidId'))
     .then((card) => {
-      if (card.owner.toString() !== userId) {
-        throw new ForbidenError('У вас нет прав на удаление данной карточки');
+      if (card.owner.toString() === userId) {
+        Card.findByIdAndRemove(cardId)
+          .then((data) => {
+            res
+              .status(200)
+              .send({ data, message: 'Карточка удалена' });
+          })
+          .catch(next);
       }
-      Card.findByIdAndRemove(cardId)
-        .then((data) => {
-          res
-            .status(200)
-            .send({ data, message: 'Карточка удалена' });
-        })
-        .catch(next);
-      // } else {
+      // else {
       // res
       //   .status(403)
       //   .send({ message: 'У вас нет прав на удаление данной карточки' });
-
+      throw new ForbidenError('У вас нет прав на удаление данной карточки');
       // }
     })
     .catch((err) => {
