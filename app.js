ac3900719b5e2256d85d05e5a2465be25cdecd06
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-22 14:01:35 +0300

    Спринт 14

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    // name: Joi.string().min(2).max(30),
-    // about: Joi.string().min(2).max(30),
-    // avatar: Joi.string(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
