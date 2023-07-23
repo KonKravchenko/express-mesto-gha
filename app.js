dc1121f994f7572cfb9158ec549c98cc27fa2c6f
Konstantin Kravchenko <kkrav911@yandex.ru>
2023-07-23 04:37:18 +0300

    Спринт 14

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 const { errors } = require('celebrate');
